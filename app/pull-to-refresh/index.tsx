8b534b4fcef50bb16bbe2a33935ae305bb2b5236
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2025-07-19 21:30:25 +0100

    Add: Pull to refresh Add: Section list

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native';
+import ThemedText from '@/components/ThemedText';
+import ThemedView from '@/components/ThemedView';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { useState } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
 
 const PullToRefreshScreen = () => {
+  const primaryColor = useThemeColor({}, 'primary');
+  const [isLoading, setLoading] = useState(false);
+
+  const onRefresh = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={isLoading} onRefresh={onRefresh} colors={[primaryColor]} />
+      }
+    >
+      <ThemedView center>
+        <ThemedText>PullToRefreshScreen</ThemedText>
+      </ThemedView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
--- app/section-list/index.tsx
+++ app/section-list/index.tsx
@@ -1,10 +1,114 @@
-import { View, Text } from 'react-native';
+import ThemedCard from '@/components/ThemedCard';
+import ThemedText from '@/components/ThemedText';
+import ThemedView from '@/components/ThemedView';
+import { SectionList } from 'react-native';
+
+interface Houses {
+  title: string;
+  data: string[];
+}
+
+const houses: Houses[] = [
+  {
+    title: 'DC Comics',
+    data: [
+      'Superman',
+      'Batman',
+      'Wonder Woman (Mujer Maravilla)',
+      'The Flash (Flash)',
+      'Aquaman',
+      'Green Lantern (Linterna Verde)',
+      'Cyborg',
+      'Shazam',
+      'Green Arrow (Flecha Verde)',
+      'Batgirl (Batichica)',
+      'Nightwing (Ala Nocturna)',
+      'Supergirl',
+      'Martian Manhunter (Detective Marciano)',
+      'Harley Quinn',
+      'Joker',
+      'Catwoman (Gata Salvaje)',
+      'Lex Luthor',
+      'Poison Ivy (Hiedra Venenosa)',
+      'Robin',
+      'Deathstroke (Deathstroke el Terminator)',
+    ],
+  },
+  {
+    title: 'Marvel Comics',
+    data: [
+      'Spider-Man (Hombre Araña)',
+      'Iron Man (Hombre de Hierro)',
+      'Captain America (Capitán América)',
+      'Thor',
+      'Black Widow (Viuda Negra)',
+      'Hulk',
+      'Doctor Strange (Doctor Extraño)',
+      'Black Panther (Pantera Negra)',
+      'Captain Marvel (Capitana Marvel)',
+      'Wolverine',
+      'Deadpool',
+      'Scarlet Witch (Bruja Escarlata)',
+      'Ant-Man (Hombre Hormiga)',
+      'Wasp (Avispa)',
+      'Groot',
+      'Rocket Raccoon (Mapache Cohete)',
+      'Gamora',
+      'Drax the Destroyer (Drax el Destructor)',
+    ],
+  },
+  {
+    title: 'Anime',
+    data: [
+      'Son Goku (Dragon Ball)',
+      'Naruto Uzumaki (Naruto)',
+      'Monkey D. Luffy (One Piece)',
+      'Sailor Moon (Sailor Moon)',
+      'Kenshin Himura (Rurouni Kenshin)',
+      'Edward Elric (Fullmetal Alchemist)',
+      'Inuyasha (Inuyasha)',
+      'Sakura Kinomoto (Cardcaptor Sakura)',
+      'Light Yagami (Death Note)',
+      'Eren Yeager (Attack on Titan)',
+      'Lelouch Lamperouge (Code Geass)',
+      'Vegeta (Dragon Ball)',
+      'Ichigo Kurosaki (Bleach)',
+      'Kaneki Ken (Tokyo Ghoul)',
+      'Gon Freecss (Hunter x Hunter)',
+      'Asuka Langley Soryu (Neon Genesis Evangelion)',
+      'Saitama (One Punch Man)',
+      'Mikasa Ackerman (Attack on Titan)',
+      'Natsu Dragneel (Fairy Tail)',
+      'Usagi Tsukino (Sailor Moon)',
+      'Sasuke Uchiha (Naruto)',
+    ],
+  },
+];
 
 const SectionListScreen = () => {
   return (
-    <View>
-      <Text>SectionListScreen</Text>
-    </View>
+    <ThemedView center>
+      <ThemedCard>
+        <SectionList
+          sections={houses}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ThemedText>{item}</ThemedText>}
+          ListHeaderComponent={() => (
+            <ThemedText className="text-2xl font-semibold mb-2">Personajes</ThemedText>
+          )}
+          renderSectionHeader={({ section }) => (
+            <ThemedText className="text-xl font-semibold my-2">{section.title}</ThemedText>
+          )}
+          ListFooterComponent={() => (
+            <ThemedText className="text-2xl font-semibold mb-2">
+              Secciones: {houses.length}
+            </ThemedText>
+          )}
+          showsVerticalScrollIndicator={false}
+          stickySectionHeadersEnabled // Mantiene el header fijado en android en ios esta por defecto con el componente
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default SectionListScreen;
