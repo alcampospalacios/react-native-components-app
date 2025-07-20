4f9cf8d33bc5806c810b11054784a0a430cc2d9b
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2025-07-20 08:20:46 +0100

    Add: Added infinite scroll

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native';
+import ThemedView from '@/components/ThemedView';
+import { useState } from 'react';
+import { ActivityIndicator, FlatList, Image, View } from 'react-native';
 
 const InfiniteScrollScreen = () => {
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+  const loadMore = () => {
+    const newArray = Array.from({ length: 10 }, (_, index) => numbers.length + (index + 1));
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 2000);
+  };
+
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
+    <ThemedView>
+      <FlatList
+        data={numbers}
+        renderItem={({ item }) => <ListItem number={item} />}
+        keyExtractor={(item) => item.toString()}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.7}
+        ListFooterComponent={() => (
+          <View className="flex justify-center items-center my-10">
+            <ActivityIndicator />
+          </View>
+        )}
+      />
+    </ThemedView>
   );
 };
 export default InfiniteScrollScreen;
+
+interface ListItemProps {
+  number: number;
+}
+
+const ListItem = ({ number }: ListItemProps) => {
+  return (
+    <Image
+      source={{
+        uri: `https://picsum.photos/id/${number}/500/400`,
+      }}
+      style={{ height: 400, width: '100%' }}
+    />
+  );
+};
